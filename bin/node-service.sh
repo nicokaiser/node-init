3675c163561b7fa0368c96a8d723fe50e1ab034f
Nico Kaiser <nico@siriux.net>
2011-10-26 10:28:28 +0200

    stderr vs stdout

--- bin/node-service.sh
+++ bin/node-service.sh
@@ -2,11 +2,12 @@
 
 PATH=$PATH:/opt/nodejs/v0.4/bin
 LOGFILE=/var/log/node-service.log
+ERRFILE=/var/log/node-service.err
 
 while [ 1 ]; do
 	echo "Restarting"
 	ulimit -n 32767
-	NODE_ENV=production node /var/www/node.example.com/current/server.js >/dev/null 2>>$LOGFILE </dev/null &
+	NODE_ENV=production node /var/www/node.example.com/current/server.js >>$LOGFILE 2>>$ERRFILE </dev/null &
 	CHILD="$!"
 	# avoid the node process to stay running after this script is terminated
 	trap "kill $CHILD; exit" exit INT TERM
