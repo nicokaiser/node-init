c68cf315dd48a57eefc184db1cd584e1e6bfda38
Nico Kaiser <nico@kaiser.me>
2014-03-15 23:15:02 +0100

    Add sleep to node-service.sh to prevent silly loops

--- bin/node-service.sh
+++ bin/node-service.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-PATH=$PATH:/opt/nodejs/v0.8/bin
+PATH=$PATH:/opt/nodejs/v0.10/bin
 LOGFILE=/var/log/node-service.log
 ERRFILE=/var/log/node-service.err
 
 while [ 1 ]; do
 	echo "Restarting"
+	sleep 2
 	ulimit -n 32767
 	NODE_ENV=production node $(dirname $0)/../server.js >>$LOGFILE 2>>$ERRFILE </dev/null &
 	CHILD="$!"
